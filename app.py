9c1de9e2371202bf3adc9314fdbcdce9fa9e3739
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 21:32:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import sys
 import os
+import sys
 
-ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOT)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 from dashboard import admin_dashboard
 
