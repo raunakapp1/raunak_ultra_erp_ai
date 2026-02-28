50e5cb94524bc56cfba19d634c8b350facfbdfa1
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 19:40:25 +0530

    Update billing_pull.py

--- billing_pull.py
+++ billing_pull.py
@@ -1,4 +1,17 @@
-import requests
+import time
+import random
+import sqlite3
 
-def fetch_billing():
-    print("Billing sync engine running...")
+conn = sqlite3.connect("database/erp.db", check_same_thread=False)
+cur = conn.cursor()
+
+def fetch_external_bills():
+    for _ in range(5):
+        cur.execute("""INSERT INTO bills
+            (guest_id,amount,platform,bill_time)
+            VALUES (?,?,?,datetime('now'))""",
+            (random.randint(1,5),
+             random.randint(120,900),
+             random.choice(["Swiggy","Zomato","POS"]),
+             ))
+    conn.commit()
