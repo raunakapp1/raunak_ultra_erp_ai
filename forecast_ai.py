34782de51ac37aedffd128d8f85c87cca10e2fa3
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 21:09:35 +0530

    Update forecast_ai.py

--- forecast_ai.py
+++ forecast_ai.py
@@ -1,10 +1,10 @@
-import random
+def predict_tomorrow_revenue(today_revenue):
+    """
+    Simple AI logic:
+    Tomorrow revenue = Today revenue + 12% growth
+    """
 
-def predict_tomorrow_revenue():
-    """
-    Simple AI forecast using random growth logic.
-    Later real ML model integrate kar sakte hain.
-    """
-    base = random.randint(25000, 60000)
-    growth = random.uniform(1.05, 1.15)
-    return int(base * growth)
+    growth_rate = 0.12
+    prediction = today_revenue + (today_revenue * growth_rate)
+
+    return round(prediction, 2)
