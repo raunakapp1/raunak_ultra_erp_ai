70de7c57b8d88f1a1a10f58d65a24fecc4a7dd9f
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 21:12:33 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import sys
+import os
+
+# Force root path add
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from ai_layer.forecast_ai import predict_tomorrow_revenue
 
 def admin_dashboard():
