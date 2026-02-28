f6a8e6366fdfde5f186a42e9816302ed21b197d1
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 21:13:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import sys
+import os
+
+# Force root path add
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from dashboard import admin_dashboard
 
 st.set_page_config(page_title="Raunak Ultra ERP AI", layout="wide")
