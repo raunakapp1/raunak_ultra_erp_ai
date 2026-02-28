e59720c4f8050fddb8ff3bb166bec95badf8b376
raunakapp1 <bikerjacket16@gmail.com>
2026-02-28 21:28:18 +0530

    Update forecast_ai.py

--- forecast_ai.py
+++ forecast_ai.py
@@ -1,10 +1,10 @@
+import random
+
 def predict_tomorrow_revenue(today_revenue):
     """
-    Simple AI logic:
-    Tomorrow revenue = Today revenue + 12% growth
+    Simple AI Forecast Logic:
+    - 8% to 18% random growth model
     """
-
-    growth_rate = 0.12
-    prediction = today_revenue + (today_revenue * growth_rate)
-
-    return round(prediction, 2)
+    growth = random.uniform(1.08, 1.18)
+    prediction = int(today_revenue * growth)
+    return prediction
